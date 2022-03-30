54bef0d75522946ecb8ef5f66e95450af1d5cc8c
bulirma <bulirma@seznam.cz>
2022-03-30 11:57:19 +0200

    init config

--- .bashrc
+++ .bashrc
@@ -2,10 +2,12 @@
 # ~/.bashrc
 #
 
-set -o vi
-
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
 # alias ls='ls --color=auto'
 # PS1='[\u@\h \W]\$ '
+
+set -o vi
+
+[ "$(tty)" = "/dev/tty1" ] && ! pidof -s Xorg >/dev/null 2>&1 && exec startx
--- .local/bin/quit.sh
+++ .local/bin/quit.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 cmd="none"
-cmd="$(printf  "shutdown\nrestart" | dmenu)"
+cmd="$(printf  "shutdown\nrestart\nexit dwm" | dmenu -p "Action:")"
 case "$cmd" in
 	"shutdown") loginctl poweroff ;;
 	"restart") loginctl reboot ;;
+	"exit dwm") kill -TERM "$(pgrep -u "$USER" "\bdwm$")" ;;
 esac
 
 exit 0
